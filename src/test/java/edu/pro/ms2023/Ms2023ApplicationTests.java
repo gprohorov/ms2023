a9b8740904556d7eff8bf90eccbadf3ea164072c
gprohorov <g.prokhorov@indevlab.com>
2023-10-05 11:22:42 +0300

    buff

--- src/test/java/edu/pro/ms2023/Ms2023ApplicationTests.java
+++ src/test/java/edu/pro/ms2023/Ms2023ApplicationTests.java
@@ -1,13 +1,19 @@
 package edu.pro.ms2023;
 
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 @SpringBootTest
 class Ms2023ApplicationTests {
 
     @Test
     void contextLoads() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        Assertions.assertNotNull(context);
     }
 
 }
